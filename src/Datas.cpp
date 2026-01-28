6b26c1842f189ae0abf17e8066e7b45f30a936c6
Matilin Periat <mperiat@emi.u-bordeaux.fr>
2026-01-28 13:19:38 +0100

    ajout gitignore, src & Datas.hpp & .cpp pour lecture d'instances

---
+++ src/Datas.cpp
@@ -0,0 +1,216 @@
+#include "Datas.hpp"
+
+/* display functions */
+
+void Familly::displayDatas() {
+
+    std::cout << "#### ENTERED FAMILLY DISPLAY FUNCTION ####" << std::endl;
+    std::cout << std::endl;
+
+    for(int i = 0; i < (int)prodToFam.size(); ++i) // prodToFam display
+        std::cout << i << "->" << "familly " << prodToFam[i] << std::endl;
+    std::cout << std::endl;
+
+    for(int i = 0; i < (int)famToProd.size(); ++i) { // famToProd display
+        std::cout << "fam : " << i << " : ";
+        for(const auto& prods : famToProd[i]) 
+            std::cout << prods << " ";  
+    }
+
+    std::cout << std::endl << std::endl;
+}
+
+void Orders::displayDatas() {
+
+    std::cout << "#### ENTERED ORDERS DISPLAY FUNCTION ####" << std::endl;
+    std::cout << std::endl;
+
+    for(int i = 0; i < (int)ordersToProduct.size(); ++i) { // orders to product display
+        std::cout << "order " << i << " : "; 
+        for(const auto& prods : ordersToProduct[i])
+            std::cout << prods << " ";
+        std::cout << std::endl; 
+    }
+
+    std::cout << std::endl;
+    
+    for(int i = 0; i < (int)productToOrders.size(); ++i) { // product to order display 
+        std::cout << "product " << i << " is in : "; 
+        for(const auto& order : productToOrders[i]) 
+            std::cout << order << " "; 
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl << std::endl;
+
+}
+
+
+void Datas::readMetaDatas(const std::string& f1) {
+
+    std::ifstream f(f1); 
+    if(!f.is_open()) 
+        throw std::runtime_error("couldn't open file");
+
+    f >> nbRacks >> nbLoc >> aeration >> nbProd >> nbFam >> nbAisle >> nbOrd; 
+
+    // resizing structures
+
+    capRacks.resize(nbRacks); 
+    dists.resize(nbRacks, std::vector<int>(nbRacks)); 
+    fam.prodToFam.resize(nbProd); 
+    fam.famToProd.resize(nbFam); 
+    ords.ordersToProduct.resize(nbOrd); 
+    ords.productToOrders.resize(nbProd); 
+    aisle.resize(nbAisle); 
+
+}   
+
+void Datas::readRackCapacity(const std::string& f2) {
+
+    std::ifstream f(f2); 
+    if(!f.is_open()) 
+        throw std::runtime_error("couldn't open file");
+
+    f >> nbRacks; 
+    for(int i = 0; i < nbRacks; ++i) 
+        f >> capRacks[i]; 
+
+}
+
+void Datas::readDistances(const std::string& f3) {
+
+    std::ifstream f(f3); 
+    if(!f.is_open())
+        throw std::runtime_error("couldn't open file"); 
+
+    int nbNodes;
+    f >> nbNodes; 
+
+    for(int i = 0; i < nbNodes; ++i) { // iterate over nb of racks both time
+        for(int j = 0; j < nbNodes; ++j) {
+            f >> dists[i][j]; 
+        }
+    }
+
+}
+
+void Datas::readProdCircuits(const std::string& f4) {
+
+    std::ifstream f(f4); 
+    if(!f.is_open())
+        throw std::runtime_error("couldn't open file"); 
+
+    f >> nbProd; 
+    
+    for(int i = 0; i < nbProd; ++i) { 
+        int currFam; // store currFamilly
+        f >> currFam; 
+        --currFam; // re indexation of Fam
+        fam.prodToFam[i] = currFam; // memorize product i is in familly currFam
+        fam.famToProd[currFam].insert(i); // memorize currFam is assigned to product i
+    }   
+
+}
+
+void Datas::readAisleRacks(const std::string& f5) {
+
+    std::ifstream f(f5); 
+    if(!f.is_open())
+        throw std::runtime_error("couldn't open file");
+        
+    f >> nbAisle; 
+    for(int i = 0; i < nbAisle; ++i) {
+        
+        int currNbRacks; 
+        int temp; 
+
+        f >> currNbRacks; 
+        aisle[i] = currNbRacks+currNbRacks*i; 
+
+        for(int j = 0; j < currNbRacks; ++j) 
+            f >> temp;
+
+    }
+
+}
+
+void Datas::readOrders(const std::string& f6) {
+
+    std::ifstream f(f6); 
+    if(!f.is_open())
+        throw std::runtime_error("couldn't open file");
+
+    f >> nbOrd; 
+    for(int i = 0; i < nbOrd; ++i) { // iterate over number of order
+        
+        int currNbProd; 
+        f >> currNbProd; // get number of products in order i
+        
+        for(int j = 0; j < currNbProd; ++j) {
+            
+            int currProduct; 
+            f >> currProduct; 
+            
+            ords.ordersToProduct[i].insert(currProduct); // memorize product is in order i 
+            ords.productToOrders[currProduct].insert(i); // memorize order i is in currProduct 
+        }
+    }
+
+}
+
+Datas::Datas(const allFiles& allfiles) {
+
+    readMetaDatas(allfiles.f1);
+    readRackCapacity(allfiles.f2); 
+    readDistances(allfiles.f3);     // call every readers
+    readProdCircuits(allfiles.f4); 
+    readAisleRacks(allfiles.f5); 
+    readOrders(allfiles.f6);
+
+}
+
+void Datas::displayDatas() {
+
+    std::cout << "#### ENTERED DISPLAY DATA FUNCTION #### " << std::endl << std::endl;
+
+    std::cout << "nb racks : " << nbRacks << std::endl;
+    std::cout << "nb locations : " << nbLoc << std::endl;
+    std::cout << "nb products : " << nbProd << std::endl;
+    std::cout << "nb familly (circuits) : " << nbFam << std::endl;
+    std::cout << "nb of aisles : " << nbAisle << std::endl;
+    std::cout << "nb of orders : " << nbOrd << std::endl;   
+    std::cout << "aeration : " << aeration << std::endl;
+    std::cout << std::endl;
+
+    // distance displays
+
+    std::cout << "#### DISTANCE DISPLAYING ####" << std::endl << std::endl;
+    for(int i = 0; i < (int)dists.size(); ++i) {
+        for(int j = 0; j < (int)dists.size(); ++j) {
+            std::cout << dists[i][j] << " "; 
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+
+    //rack cap display 
+
+    std::cout << "#### RACKS CAPACITY ####" << std::endl << std::endl;
+    for(const auto& cap : capRacks)
+        std::cout << cap << " "; 
+    std::cout << std::endl << std::endl;
+
+    // aisle display 
+
+    std::cout << "#### DISPLAY AISLE RACK last id ####" << std::endl << std::endl;
+    for(const auto& rack : aisle) 
+        std::cout << rack << " "; 
+    std::cout << std::endl << std::endl;
+
+    fam.displayDatas(); 
+    ords.displayDatas(); 
+
+}
+
+
---
+++ src/Datas.hpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <fstream>
+
+struct Familly { // will contain infos relatives to fam and products 
+    std::vector<int> prodToFam; 
+    std::vector<std::unordered_set<int>> famToProd; 
+    void displayDatas(); 
+}; 
+
+struct Orders {
+    std::vector<std::unordered_set<int>> ordersToProduct; // fastly check products associated to orders
+    std::vector<std::unordered_set<int>> productToOrders; // fastly check orders associated to product
+    void displayDatas(); 
+};
+
+struct allFiles {
+    std::string f1,f2,f3,f4,f5,f6; 
+};
+
+struct Datas {
+
+    /* ATTRIBUTS */
+
+    int nbRacks, nbLoc, nbProd, nbFam, nbAisle, nbOrd;
+    int aeration; 
+
+    std::vector<int> capRacks; // for racks capacity
+    std::vector<std::vector<int>> dists; // distances 
+    std::vector<int> aisle; 
+
+    Familly fam;  
+    Orders ords; 
+
+    /* METHODS */
+
+    Datas(const allFiles& allfiles); 
+
+    void readMetaDatas(const std::string& f1); // read meta datas
+    void readRackCapacity(const std::string& f2); 
+    void readDistances(const std::string& f3); 
+    void readProdCircuits(const std::string& f4); 
+    void readAisleRacks(const std::string& f5); 
+    void readOrders(const std::string& f6); 
+
+    void displayDatas(); 
+
+}; 
